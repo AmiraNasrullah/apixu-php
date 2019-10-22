85e33faf13373a591c58f51ef5439b38ca1ed3e1
Amira Nasrullah <amira.nasrullah@almtar.com>
2019-10-22 15:16:55 +0200

    [Update] apixu base url

--- src/Config.php
+++ src/Config.php
@@ -5,7 +5,7 @@ namespace Apixu;
 class Config
 {
     const FORMAT = 'json';
-    const API_URL = 'https://api.apixu.com/v%s/%s.%s?key=%s%s';
+    const API_URL = 'http://api.weatherstack.com/%s?access_key=%s%s';
     const API_VERSION = '1';
     const DOC_WEATHER_CONDITIONS_URL = 'https://www.apixu.com/doc/Apixu_weather_conditions.%s';
     const MAX_QUERY_LENGTH = 100;
